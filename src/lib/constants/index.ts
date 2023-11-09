b5c08b675f4a0d61bc0c2874f642dae121fd4a24
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-09 07:53:14 +0300

    add constants

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,13 @@
+const ROUTER = {
+  HOME: '/',
+  DASHBOARD: '/dashboard',
+  USERS: '/users',
+  USER: '/users/id',
+  ADD_USER: '/users/add',
+  PRODUCTS: '/products',
+  PRODUCT: '/products/id',
+  ADD_PRODUCT: '/products/add',
+  LOGIN: '/login',
+}
+
+export { ROUTER }
