f2725cd13cef1a4bd1722e4e08e0b53a512dfa4c
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-09 07:50:10 +0300

    create sidebar component

---
+++ src/layout/sidebar/sidebar.tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image'
+import { MenuLink } from '@/ui/menuLink'
+import styles from './sidebar.module.css'
+import { menuItems } from '@/lib/constants'
+import { MdLogout } from 'react-icons/md'
+
+// import { auth, signOut } from '@/app/auth'
+
+const Sidebar = async () => {
+  // const { user } = await auth()
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.user}>
+        <Image
+          className={styles.userImage}
+          // src={user.img || '/noavatar.png'}
+          src={'/noavatar.png'}
+          alt=''
+          width='50'
+          height='50'
+        />
+        <div className={styles.userDetail}>
+          {/* <span className={styles.username}>{user.username}</span> */}
+          <span className={styles.username}>Username</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+
+      <ul className={styles.list}>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink item={item} key={item.title} />
+            ))}
+          </li>
+        ))}
+      </ul>
+
+      <form
+        action={async () => {
+          'use server'
+          // await signOut()
+        }}
+      >
+        <button className={styles.logout}>
+          <MdLogout />
+          Logout
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export { Sidebar }
