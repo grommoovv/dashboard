de51e6a6fef914bf267b71eb69cc62c1aed0ce21
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-09 07:50:42 +0300

    create footer component

---
+++ src/layout/footer/footer.tsx
@@ -0,0 +1,11 @@
+import styles from './footer.module.css'
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.wrap}></div>
+    </div>
+  )
+}
+
+export { Footer }
