049d0d3d699db2524334d34bc39988998c52fbb9
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-09 07:55:34 +0300

    dashboard page

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,27 @@
+import { Rightbar } from '@/layout/rightbar'
+import { Card } from '@/ui/card/card'
+import { Chart } from '@/ui/chart/chart'
+import { Transactions } from '@/widgets/transactions'
+import styles from './page.module.css'
+import { cardsInfo } from '@/lib/constants'
+
+const Page = () => {
+  return (
+    <div className={styles.page}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          {cardsInfo.map((item) => (
+            <Card item={item} key={item.id} />
+          ))}
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  )
+}
+
+export default Page
